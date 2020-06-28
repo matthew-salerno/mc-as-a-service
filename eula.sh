75f319aa58d619ac611f9bdd92d222671d2c9dfb
Matthew Salerno <mattjs@vt.edu>
2020-06-28 14:50:48 -0400

    maybe path variables are fixed for real?

--- eula.sh
+++ eula.sh
@@ -2,7 +2,7 @@
 manifest_url="https://launchermeta.mojang.com/mc/game/version_manifest.json"
 config_path="$SNAP""/etc/mc-as-a-service.json"
 server_path="`cat "$config_path" | jq -r '.launcher.server_path | @sh' | sed "s/^'//" | sed "s/'$//"`"
-eula_path= $server_path"/eula.txt"
+eula_path=$server_path"/eula.txt"
 ##EULA
 echo "#By changing the setting below to TRUE you are indicating your agreement to our EULA (https://account.mojang.com/documents/minecraft_eula)." > $eula_path
 echo "#""`date`" >> $eula_path
