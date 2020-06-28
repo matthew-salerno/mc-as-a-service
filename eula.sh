5ef98390a61e9376ac197a4df5edac50c3eb9a2d
Matthew Salerno <mattjs@vt.edu>
2020-06-28 12:38:26 -0400

    added tui installer

---
+++ eula.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+manifest_url="https://launchermeta.mojang.com/mc/game/version_manifest.json"
+config_path="$SNAP""/etc/mc-as-a-service.json"
+server_path=`cat "$config_path" | jq '.launcher.server_path'` 
+eula_path= "$server_path""/eula.txt"
+##EULA
+echo "#By changing the setting below to TRUE you are indicating your agreement to our EULA (https://account.mojang.com/documents/minecraft_eula)." > $eula_path
+echo "#""`date`" >> $eula_path
+if (whiptail --title "EULA" --yesno --scrolltext "YOU MUST AGREE TO THE EULA TO RUN A MINECRAFT SERVER\nPlease read the EULA in its entirety, selecting yes indicates that you agree to the EULA:\n""`lynx --dump --anonymous https://account.mojang.com/documents/minecraft_eula | sed '/^ *\*\|^ *+/d' | sed '1,+3d'`" 30 78); then
+    echo "eula=true" >> $eula_path
+    echo "set eula to true"
+else
+    echo "eula=false" >> $eula_path
+    echo "set eula to false"
+    whiptail --title "EULA" --msgbox "You must agree to the EULA to to use this software" 30 78
+fi
