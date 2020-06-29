ff2f55b4484879cc80399f62246da7133db6c1f3
Matthew Salerno <mattjs@vt.edu>
2020-06-28 20:12:45 -0400

    typo

--- header.sh
+++ header.sh
@@ -7,8 +7,8 @@ export manifest_url="https://launchermeta.mojang.com/mc/game/version_manifest.js
 export eula_path=$server_path"/eula.txt"
 export server_jar_path="$server_path""/server.jar"
 export jarfile_path="$SNAP_USER_DATA""$server_path""`cat "$config_path" | jq -r '.launcher.jarfile | @sh' | sed "s/^'//" | sed "s/'$//"`"
-export $in_pipe="$server_path""/inpipe"
-export $out_pipe="$server_path""/outpipe"
+export in_pipe="$server_path""/inpipe"
+export out_pipe="$server_path""/outpipe"
 
 #make sure config directory exists
 if [ ! -d "$SNAP_USER_DATA""/config" ]; then
