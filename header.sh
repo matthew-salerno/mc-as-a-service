b3b90585272414019b7f5dd59425e5a651d01908
Matthew Salerno <mattjs@vt.edu>
2020-06-28 20:51:01 -0400

    running out of options

--- header.sh
+++ header.sh
@@ -20,7 +20,7 @@ if [ ! -d $server_path ]; then
 fi
 
 export jarfile_path="$SNAP_USER_DATA""$server_path""`cat "$config_path" | jq -r '.launcher.jarfile | @sh' | sed "s/^'//" | sed "s/'$//"`"
-export eula_path=$server_path"/eula.txt"
+export eula_path="$server_path""/eula.txt"
 export server_jar_path="$server_path""/server.jar"
 export manifest_url="https://launchermeta.mojang.com/mc/game/version_manifest.json"
 export in_pipe="$server_path""/inpipe"
