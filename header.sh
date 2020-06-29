4743436c0efe876d50c39dc33c0b515e1654d9c7
Matthew Salerno <mattjs@vt.edu>
2020-06-28 20:43:16 -0400

    still trying to fix eula

--- header.sh
+++ header.sh
@@ -2,13 +2,6 @@
 #Common variables used by all programs
 
 export config_path="$SNAP_USER_DATA""/config/mc-as-a-service.json"
-export server_path="$SNAP_USER_DATA""`cat "$config_path" | jq -r '.launcher.server_path | @sh' | sed "s/^'//" | sed "s/'$//"`" 
-export manifest_url="https://launchermeta.mojang.com/mc/game/version_manifest.json"
-export eula_path=$server_path"/eula.txt"
-export server_jar_path="$server_path""/server.jar"
-export jarfile_path="$SNAP_USER_DATA""$server_path""`cat "$config_path" | jq -r '.launcher.jarfile | @sh' | sed "s/^'//" | sed "s/'$//"`"
-export in_pipe="$server_path""/inpipe"
-export out_pipe="$server_path""/outpipe"
 
 #make sure config directory exists
 if [ ! -d "$SNAP_USER_DATA""/config" ]; then
@@ -16,9 +9,19 @@ if [ ! -d "$SNAP_USER_DATA""/config" ]; then
 fi
 #make sure config exists
 if [ ! -f $config_path ]; then
-    cp "$SNAP""/etc/default_config.json" "$config_path"
+    cp "$SNAP""/etc/default-config.json" "$config_path"
 fi
+
+export server_path="$SNAP_USER_DATA""`cat "$config_path" | jq -r '.launcher.server_path | @sh' | sed "s/^'//" | sed "s/'$//"`" 
+
 #make sure server folder exists
 if [ ! -d $server_path ]; then
     mkdir $server_path
 fi
+
+export jarfile_path="$SNAP_USER_DATA""$server_path""`cat "$config_path" | jq -r '.launcher.jarfile | @sh' | sed "s/^'//" | sed "s/'$//"`"
+export eula_path=$server_path"/eula.txt"
+export server_jar_path="$server_path""/server.jar"
+export manifest_url="https://launchermeta.mojang.com/mc/game/version_manifest.json"
+export in_pipe="$server_path""/inpipe"
+export out_pipe="$server_path""/outpipe"
