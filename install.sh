a108ed2aad0b360ca46f0c2056737e298ffa5e00
Matthew Salerno <mattjs@vt.edu>
2020-06-28 21:16:27 -0400

    Some progress! I made eula a function

--- install.sh
+++ install.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 bash "$SNAP""/bin/header.sh"
+
+#EULA FUNCTION
+##EULA
+function eula () {
+    echo "Eula path: \"""$eula_path""\""
+    echo "#By changing the setting below to TRUE you are indicating your agreement to our EULA (https://account.mojang.com/documents/minecraft_eula)." > "$eula_path"
+    echo "#""`date`" >> "$eula_path"
+    echo $(lynx --cfg "$SNAP""/etc/lynx/lynx.cfg" --dump --anonymous "https://account.mojang.com/documents/minecraft_eula" | sed '/^ *\*\|^ *+/d' | sed '1,+4d')
+    if (whiptail --title "EULA" --yesno --scrolltext "YOU MUST AGREE TO THE EULA TO RUN A MINECRAFT SERVER\nPlease read the EULA in its entirety, selecting yes indicates that you agree to the EULA:\n\n""`lynx --cfg "$SNAP""/etc/lynx/lynx.cfg" --dump --anonymous "https://account.mojang.com/documents/minecraft_eula" | sed '/^ *\*\|^ *+/d' | sed '1,+2d'`" 30 78); then
+        echo "eula=true" >> "$eula_path"
+        return 0
+    else
+        echo "eula=false" >> "$eula_path"
+        return 1
+        whiptail --title "EULA" --msgbox "You must agree to the EULA to to use this software" 30 78
+    fi
+}
+
 ##ignore eula if it is already filled out
 #eula starts out as false
 eula="0"
@@ -10,7 +28,8 @@ if [ -f "$eula_path" ]; then
 fi
 
 if [ $eula = "0" ]; then
-    if [[ `bash "$SNAP""/bin/eula.sh"` =~ .*false ]]; then
+    eula
+    if [[ $? = 1 ]]; then
         exit 0
     fi
 fi
