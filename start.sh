f4cf281db00399b925cb06d8b3a44e83cf42621f
Matthew Salerno <mattjs@vt.edu>
2020-06-28 22:02:34 -0400

    added java paths

--- start.sh
+++ start.sh
@@ -3,6 +3,8 @@ source "$SNAP"/bin/header.sh
 #load the configuration
 mem_min=$(cat "$config_path" | jq -r '.launcher.memory.min | @sh' | sed "s/^'//" | sed "s/'$//")
 mem_max=$(cat "$config_path" | jq -r '.launcher.memory.max | @sh' | sed "s/^'//" | sed "s/'$//")
+export JAVA_HOME="$SNAP""/usr/lib/jvm/java-1.8.0-openjdk-$SNAP_ARCH"
+export PATH="$JAVA_HOME""/bin:$JAVA_HOME/jre/bin:$PATH"
 
 #create pipes if none exists
 if [ ! -f "inpipe" ]
