901123506ac3a9b3a95faf7879b220c20607cff7
Matt <mattjs@vt.edu>
2020-07-09 22:42:12 -0400

    Can now select a branch in version_selector

--- helpers/version_selector.py
+++ helpers/version_selector.py
@@ -9,18 +9,60 @@ import urllib3
 const = shared.constants()
 https = urllib3.PoolManager()
 
-def select(branch="all"):
+def select(branch="ask"):
+    if branch == "ask":
+        branch = curses.wrapper(select_branch)
     versions = version_list(branch)
     return curses.wrapper(versions.display)
-class version_list(branch="all"):
-    def __init__(self):
+    
+def select_branch(stdscr):
+    rows, cols = stdscr.getmaxyx()
+    curses.noecho()
+    curses.cbreak()
+    curses.curs_set(0)
+    selected=0
+    branches=["Release","Snapshot","All"]
+    while True:
+        stdscr.erase()
+        for i in range(len(branches)):
+            # Get centered position
+            half_length_of_message = int(len(" ".join(branches)) / 2)
+            
+            middle_column = int(cols / 2)
+            middle_row = int(rows / 2)
+            y_position = middle_row
+            x_position = middle_column - half_length_of_message
+            # Print
+            if selected == i:
+                stdscr.addstr(y_position, x_position +
+                (sum(map(len,branches[0:i])))+i, branches[i], curses.A_STANDOUT)
+            else:
+                stdscr.addstr(y_position, x_position +
+                (sum(map(len,branches[0:i])))+i, branches[i])
+        stdscr.refresh()
+        key = stdscr.getch()
+        if key == curses.KEY_RIGHT:
+            selected = (selected + 1)%len(branches)
+        elif key == curses.KEY_LEFT:
+            selected = (selected - 1)%len(branches)
+        elif key == curses.KEY_ENTER or key == 10 or key == 13:
+                    return branches[selected]
+    
+
+class version_list():
+    def __init__(self, branch="all"):
         self._version_list=[]
         self._selected = 0
-        
         manifest = json.loads(https.request('GET',const.MANIFEST_URL).data.decode("utf-8"))
         for version in manifest["versions"]:
-            self._version_list.append(version["id"])
-
+            if branch == "all":
+                self._version_list.append(version["id"])
+            elif branch == "snapshot":
+                if version["type"] == "snapshot":
+                    self._version_list.append(version["id"])
+            elif branch == "release":
+                if version["type"] == "release":
+                    self._version_list.append(version["id"])
     def display(self, stdscr):
         rows, cols = stdscr.getmaxyx()
         curses.noecho()
@@ -69,3 +111,6 @@ class version_list(branch="all"):
             self._selected += 1
 if __name__ == "__main__":
     print(select())
+    print(select("all"))
+    print(select("release"))
+    print(select("snapshot"))
